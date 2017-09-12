91f33bb293551bb4ba98cff49362a5a9c2db1dd3
shy <1430954136@qq.com>
2017-09-12 21:35:29 +0800

    提交

---
+++ shop/Application/Model/MembersModel.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/12
+ * Time: 11:15
+ */
+class MembersModel extends Model
+{
+    public  function check($username,$password){
+
+    }
+}
